779e54da10f51864892b5ecb152e2209ff245863
edujime23 <psnedujime@gmail.com>
2025-08-18 16:54:56 -0600

    testing what kind of acces vercel has over the repo

--- server/api/index.py
+++ server/api/index.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 # Add strict_slashes=False to make the trailing slash optional
 @app.route('/', strict_slashes=False)
 def hello_world():
-    return 'Hello, World!'
+    return os.listdir("../..")
 
 @app.route('/about', strict_slashes=False)
 def about_page():
