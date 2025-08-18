27977d2db44a00ac7c1d6848b955b61d6821adb0
edujime23 <psnedujime@gmail.com>
2025-08-17 21:55:51 -0600

    fixing versel again

--- server/api/test.py
+++ server/api/test.py
@@ -1,19 +1,12 @@
 from flask import Flask
 
-# Vercel (and other platforms) will look for an object named 'app'.
 app = Flask(__name__)
 
-# This is a "decorator" that tells Flask:
-# When someone visits the main URL ('/'), run the function below.
-@app.route('/')
+# Add strict_slashes=False to make the trailing slash optional
+@app.route('/', strict_slashes=False)
 def hello_world():
-    # The string returned by this function is what the user will see in their browser.
     return 'Hello, World!'
 
-# You can add more routes for different pages
-@app.route('/about')
+@app.route('/about', strict_slashes=False)
 def about_page():
     return 'This is the About Page.'
-
-# Note: The 'if __name__ == "__main__":' block for running locally
-# is NOT needed for Vercel deployment, as Vercel handles running the app itself.
