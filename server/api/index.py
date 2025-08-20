5f216718dcd260528e24df62a05e730a5e958741
edujime23 <psnedujime@gmail.com>
2025-08-19 18:56:02 -0600

    test

--- server/api/index.py
+++ server/api/index.py
@@ -1,18 +1,18 @@
 # server/api/index.py
 import os
 import json
-from flask import Flask, Response, abort
+from flask import Flask, Response, abort, request
 from pathlib import PurePosixPath
 
 app = Flask(__name__)
 
 # --- Routes ---
 
-@app.route('/music/', defaults={'subpath': ''}, strict_slashes=False)
-@app.route('/music/<path:subpath>', strict_slashes=False)
-def browse_music(subpath):
-    if subpath:
-        return os.listdir(subpath)
+@app.route('/music', strict_slashes=False)
+def browse_music():
+    path = request.args.get('path', os.getcwd())
+    if path:
+        return os.listdir(path)
     
 
 @app.route('/', strict_slashes=False)
